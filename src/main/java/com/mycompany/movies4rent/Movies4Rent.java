d2a235266539f5588eb58f5ee4309e5ed8e707d7
MSI-CARLOS\Carlos <carlmada@hotmail.com>
2023-03-06 14:49:50 +0100

    version1

---
+++ src/main/java/com/mycompany/movies4rent/Movies4Rent.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.movies4rent;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Movies4Rent {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+    }
+}
